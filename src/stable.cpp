21f99527149734f67e834e81030118a10b433b6a
Ethan Rutherford <ethanrutherford2@yahoo.com>
2014-11-11 11:02:55 -0600

    create stable

---
+++ src/stable.cpp
@@ -0,0 +1,49 @@
+#include "stable.h"
+using namespace ramsey;
+
+void stable::addScope()
+{
+	table.emplace_back();
+}
+
+void stable::remScope()
+{
+	table.pop_back();
+}
+
+bool stable::add(std::string name, stype type)
+{
+	if (table.back().find(name) != table.back().end())
+		return false;
+	symbol s;
+	s.name = name;
+	s.type = type;
+	table.back().insert(s);
+	return true;
+}
+
+bool stable::hasSymbol(std::string name)
+{
+	if (!find(name))
+		return true;
+	return false;
+}
+
+stype stable::typeOf(std::string name)
+{
+	if (!find(name))
+		return stype_invalid;
+	return find(name)->type;
+}
+
+const symbol* stable::find(std::string name)
+{
+	for (auto it = table.rbegin(); it != table.rend(); it++)
+	{
+		auto it2 = it->find(name);
+		if (it2 != it->end())
+			return &*it2;
+	}
+	return NULL;
+}
+
---
+++ src/stable.h
@@ -0,0 +1,44 @@
+#ifndef STABLE_H
+#define STABLE_H
+
+#include <unordered_set>
+#include <deque>
+#include <string>
+
+namespace ramsey
+{
+	enum stype {stype_invalid = -1, stype_in, stype_boo};
+	struct symbol
+	{
+		symbol() {}
+		symbol(std::string n) : name(n) {}
+		std::string name;
+		stype type;
+	};
+	bool operator==(symbol a, symbol b)
+	{
+		return a.name == b.name;
+	}
+}
+namespace std{template<>
+	struct hash<ramsey::symbol>{
+	size_t operator()(const ramsey::symbol &a ) const
+	{return hash<std::string>()(a.name);}
+};}
+namespace ramsey
+{
+	class stable
+	{
+	public:
+		void addScope();
+		void remScope();
+		bool add(std::string name, stype type);
+		bool hasSymbol(std::string name);
+		stype typeOf(std::string name);
+	private:
+		std::deque<std::unordered_set<symbol> > table;
+		const symbol* find(std::string name);
+	};
+}
+
+#endif
